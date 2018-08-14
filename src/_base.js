521aa20bba7737c3c5d8ef6a56f0e107673e2089
Rainbow <35144413+rainbownotfound@users.noreply.github.com>
2018-08-14 21:45:25 +0200

    Uploaded 1 file

---
+++ src/_base.js
@@ -0,0 +1,12 @@
+// Dependencies Here
+
+module.exports.run = (client, message, args) => {
+    // Command Code Here
+};
+
+module.exports.help = {
+    command: 'command',
+    usage: 'usage',
+    category: 'category',
+    description: 'description'
+};
